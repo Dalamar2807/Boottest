432b4f2b30080e58b0c3b5b704ad3735f918e630
Dalamar2807 <dam.montolieu@gmail.com>
2020-07-19 14:21:27 +0100

    Deuxième commit avec new App Compo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CalltoactionComponent } from './calltoaction/calltoaction.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    CalltoactionComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
